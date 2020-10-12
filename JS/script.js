c00094456ead15bb102bbcddc194a97b65d7dcbe
Ryan <ryankirk1331@gmail.com>
2020-10-12 12:01:58 -0400

    fifth commit

--- JS/script.js
+++ JS/script.js
@@ -16,18 +16,32 @@ $(document).ready(function() {
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        items: 4,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            480:{
+                items:2
+            },
+            768:{
                 items:3
             },
-            1000:{
-                items:5
+            938:{
+                items:4
             }
+        }
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
         }
     });
 
