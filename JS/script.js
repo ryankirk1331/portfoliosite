b31dd17a57b8b6c11a2001c9635e495cb1321362
Ryan <ryankirk1331@gmail.com>
2020-10-12 07:59:32 -0400

    third commit

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
         play: 5000
     });
 
+    var typed = new Typed(".typed",  {
+        strings: ["Javascript Devoloper.", "Web Development.", "Web Design."],
+        typeSped: 110,
+        loop: true,
+        startDelay: 2000,
+        showCursor: false
+    });
+
 });
