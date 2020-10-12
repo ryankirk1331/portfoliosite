e36a68ab4c859891b8c006658b6ceaa85538e401
Ryan <ryankirk1331@gmail.com>
2020-10-12 10:54:30 -0400

    fourth commit

--- JS/script.js
+++ JS/script.js
@@ -6,10 +6,29 @@ $(document).ready(function() {
 
     var typed = new Typed(".typed",  {
         strings: ["Javascript Devoloper.", "Web Development.", "Web Design."],
-        typeSped: 110,
+        typeSpeed: 70,
         loop: true,
-        startDelay: 2000,
+        startDelay: 1020,
         showCursor: false
+    });
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     });
 
 });
