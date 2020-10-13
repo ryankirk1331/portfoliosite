1c32fef3e8376babafaaf443ef586249bb5281e0
Ryan <ryankirk1331@gmail.com>
2020-10-13 09:54:38 -0400

    commit before stats

--- JS/script.js
+++ JS/script.js
@@ -31,15 +31,22 @@ $(document).ready(function() {
         }
     });
 
-    $('.chart').easyPieChart({
-        easing: 'easeInOut',
-        barColor: '#fff',
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 152,
-        onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
+    var skillsTopOffset = $(".skillSection").offset().top;
+
+    $(window).scroll(function() {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        
         }
     });
     
