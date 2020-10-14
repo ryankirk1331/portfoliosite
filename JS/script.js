3eee201945d912f68e73384e35b8791369499bbf
Ryan <ryankirk1331@gmail.com>
2020-10-14 17:10:53 -0400

    commit before copywrite

--- JS/script.js
+++ JS/script.js
@@ -59,7 +59,26 @@ $(document).ready(function() {
             easing: 'linear',
             queue: false
         }
+    });
+
+    $("#filters a").click(function(){
+
+        $("#filters.current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        
+        return false;
+    
     })
 
-    
 });
