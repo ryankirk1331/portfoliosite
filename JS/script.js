eadd56f684ed471b917ebc7080241300d3d82f1e
Ryan <ryankirk1331@gmail.com>
2020-10-15 10:55:01 -0400

    end of class

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
+$(window).on("load", function() {
+
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
+    });
+
+})
+
 $(document).ready(function() {
     $('#slides').superslides({
         animation: 'fade',
